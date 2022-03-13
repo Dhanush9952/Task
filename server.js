ce4afd97a393912eabd35906305e02a2b2f417c7
Dhanush <dhanushkrish45@gmail.com>
2022-03-13 21:14:48 +0530

    Deploy to Heroku commit-2

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/task'));
+app.use(express.static('./dist/TaskView'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/task/'}),
+    res.sendFile('index.html', {root: 'dist/ATask/'}),
 );
 
 // Start the app by listening on the default Heroku port
